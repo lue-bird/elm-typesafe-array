68a1cd0ee09ab713f374ca4b1c4d0be42a830a4f
passi <pascalfeyer@gmail.com>
2021-04-21 12:22:00 +0200

    - reverse note

--- src/ArrayExtra.elm
+++ src/ArrayExtra.elm
@@ -3,8 +3,6 @@ module ArrayExtra exposing (reverse)
 import Array exposing (Array)
 
 
-{-| Do you know a better way?
--}
 reverse : Array a -> Array a
 reverse =
     Array.toList >> List.reverse >> Array.fromList
